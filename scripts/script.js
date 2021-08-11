73ca7ffde7b4e88e5bdd44143de4d20444ef9505
Petrick Fontamillas <petrick@Iphone.local>
2021-08-11 17:23:44 +0800

    Navbar - Done; Section Hero - Done; Section System - Done; Section Cards - Done; Section about - Dne; Section Services - Done; Section Premium - Done; Section Contact - Polishing;

--- scripts/script.js
+++ scripts/script.js
@@ -14,5 +14,14 @@ var headerScrolled = () => {
   });
 }
 
+var subMenu = () => {
+  $('.btn-toogle').click(function(){
+    $(this).toggleClass("click");
+    // $('.sidebar').toggleClass("show");
+    $('body').toggleClass("show");
+  });
+}
+
 // initialize the functions
 headerScrolled();
+subMenu();
